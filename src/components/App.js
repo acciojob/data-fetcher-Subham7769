fa42260d6d0b451a49e4ff283dac77c173bc538f
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-19 14:14:54 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,31 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
-import { error } from "cypress/types/jquery";
+
 
 const App = () => {
 
 
-  const [data , setData] = useState(null);
-  try {
-    
-    fetchData('https://dummyjson.com/products')
-    .then(response=>response.json())
-    .then(response=>setData(response.data))
-  } catch (error) {
-    console.log(error)
+  const [data , setData] = useState([]);
+
+  function fetchData(){
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(response => {
+        setData(response.products);
+      })
+      .catch(error => console.log(error));
   }
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Data fetched from API</h1>
-        <p>{data}</p>
+        <pre>{JSON.stringify(data)}</pre>
     </div>
   )
 }
